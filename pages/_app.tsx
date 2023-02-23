32d6c3a228cc16c5898f3bc3a7217813f080bc20
Yassin Hussein <4assin@gmail.com>
2023-02-23 18:22:29 +0200

    add coming soon page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Baloo_Bhaijaan_2 } from "@next/font/google";
+
+const NotoFont = Baloo_Bhaijaan_2({
+  subsets: ["arabic"],
+  weight: ["400", "500", "600", "700", "800"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={NotoFont.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
